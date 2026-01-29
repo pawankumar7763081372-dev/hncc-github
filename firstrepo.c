0d0da3ec8b8b565c019e0ee75841d8f907db2f8d
pawan kumar <pawankumar7763081372@gmail.com>
2026-01-29 22:56:14 +0530

    my first commit

---
+++ firstrepo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("my first file in c programming\n");
+    printf("uploaded in github\n");
+    printf("my name is pawan kumar agarwal\n");
+
+    return 0;
+}
